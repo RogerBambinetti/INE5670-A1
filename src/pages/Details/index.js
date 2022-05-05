643559c21da7075ef78844725ef302b554739421
roger <roger.bambinetti@ozmap.com.br>
2022-05-05 02:12:16 -0300

    add image changing

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -14,6 +14,7 @@ export default function Details() {
   const item = route.params.item;
 
   const [favorite, setFavorite] = useState(false);
+  const [pictureIndex, setPictureIndex] = useState(0);
 
   useEffect(async () => {
     const favorites = await AsyncStorage.getItem('favorites') || {};
@@ -33,16 +34,21 @@ export default function Details() {
     }
   }
 
+  function handlePictureChange() {
+    const value = pictureIndex == (item.pictures.length - 1) ? 0 : (pictureIndex + 1);
+    setPictureIndex(value);
+  }
+
   return (
     <LinearGradient colors={['#2DDCEB', '#35DE8C']} style={styles.container}>
       <Menu title="Detalhes" />
-      <View style={styles.imageContainer}>
+      <TouchableOpacity style={styles.imageContainer} onPress={handlePictureChange}>
         <LinearGradient colors={['transparent', 'rgba(0,0,0,0.5)']} style={styles.overlay} />
-        <Image style={styles.image} source={{ uri: "https://www.plantapronta.com.br/projetos/161/02.jpg" }} />
+        <Image style={styles.image} source={{ uri: item.pictures[pictureIndex] }} />
         <TouchableOpacity style={styles.favorite} onPress={handleFavorite}>
           <MaterialCommunityIcons name={favorite ? "heart" : "heart-outline"} size={35} color={favorite ? "#F56459" : "#D9D9D9"} />
         </TouchableOpacity>
-      </View>
+      </TouchableOpacity>
       <View style={styles.content}>
         <Text>{item.name}</Text>
         <Text>{item.address}</Text>
